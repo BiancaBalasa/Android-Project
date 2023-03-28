928a3371a3b27a1982c83dd41532a5e28553dc64
Bianca Balasa <bianca.balasa@cst.ro>
2023-03-28 15:55:57 +0300

    wip

--- app/src/main/java/com/example/todolists/LoginActivity.java
+++ app/src/main/java/com/example/todolists/LoginActivity.java
@@ -59,7 +59,7 @@ public class LoginActivity extends AppCompatActivity {
                 .build();
          gsc = GoogleSignIn.getClient(this, gso);
 
-         //if you didn't logout than go directly to profile activity
+         //if you didn't logout than go directly to profile activity(google login)
         GoogleSignInAccount account=GoogleSignIn.getLastSignedInAccount(this);
         if(account!=null){
             Intent intent =new Intent(LoginActivity.this,ProfileActivity.class);
